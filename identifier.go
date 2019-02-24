6e9f9f1381d3218a089ec45b6721bba1ee6e0431
xDashh <Dashh@protonmail.com>
2019-02-24 02:10:34 +0100

    update embed regex

--- identifier.go
+++ identifier.go
@@ -15,10 +15,11 @@ var (
 	twitchVodRe = regexp.MustCompile(fmt.Sprintf("twitch\\.tv/videos/%s", commonMatch))
 	// twitchVodRe collides with twitchRe so additional constraint for string to end...
 	twitchRe   = regexp.MustCompile(fmt.Sprintf("twitch\\.tv/%s/?$", commonMatch))
-	atRe       = regexp.MustCompile(fmt.Sprintf("angelthump\\.com/embed/%s", commonMatch))
-	youtubeRe1 = regexp.MustCompile(fmt.Sprintf("youtube\\.com/watch.*?[&?]v=%s", commonMatch))
-	youtubeRe2 = regexp.MustCompile(fmt.Sprintf("youtu\\.be/%s", commonMatch))
-	youtubeRe3 = regexp.MustCompile(fmt.Sprintf("youtube\\.com/embed/%s", commonMatch))
+	atRe       = regexp.MustCompile(fmt.Sprintf("angelthump\\.com/(?:embed/)?%s", commonMatch))
+	atRe2      = regexp.MustCompile(fmt.Sprintf("player\\.angelthump\\.com/.*?[&?]channel=%s", commonMatch))
+	ytRe1      = regexp.MustCompile(fmt.Sprintf("youtube\\.com/watch.*?[&?]v=%s", commonMatch))
+	ytRe2      = regexp.MustCompile(fmt.Sprintf("youtu\\.be/%s", commonMatch))
+	ytRe3      = regexp.MustCompile(fmt.Sprintf("youtube\\.com/embed/%s", commonMatch))
 	facebookRe = regexp.MustCompile(fmt.Sprintf("facebook\\.com/.*?/videos/%s/?", commonMatch))
 	mixerRe    = regexp.MustCompile(fmt.Sprintf("mixer\\.com/(?:embed/player/)?%s$", commonMatch))
 
@@ -27,9 +28,10 @@ var (
 		twitchVodRe: "twitch-vod",
 		twitchRe:    "twitch",
 		atRe:        "angelthump",
-		youtubeRe1:  "youtube",
-		youtubeRe2:  "youtube",
-		youtubeRe3:  "youtube",
+		atRe2:       "angelthump",
+		ytRe1:       "youtube",
+		ytRe2:       "youtube",
+		ytRe3:       "youtube",
 		facebookRe:  "facebook",
 		mixerRe:     "mixer",
 	}
