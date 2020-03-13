44a4124f7b94919e1f6643aa45fa10629ee42801
jbpratt78 <jbpratt78@gmail.com>
2020-03-13 17:30:18 -0500

    going for everything after 'CHANGELOG' in commit messages

--- changelog.go
+++ changelog.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"regexp"
 	"strings"
 
 	"gopkg.in/src-d/go-git.v4"
@@ -19,15 +18,16 @@ func getLatestChanges(repo string, n int) ([]string, error) {
 		return nil, fmt.Errorf("failed 'git log': %v", err)
 	}
 
-	re := regexp.MustCompile(`\r?\n`)
 	for i := 0; i < n; i++ {
 		com, err := cIter.Next()
 		if err != nil {
 			return nil, fmt.Errorf("failed to get next commit log: %v", err)
 		}
-		// strip all new lines, replace with spaces
-		// trim and extra leading or trailing spaces
-		msgs = append(msgs, strings.TrimSpace(re.ReplaceAllString(com.Message, " ")))
+
+		if strings.Contains(com.Message, "CHANGELOG") {
+			msgs = append(msgs,
+				strings.TrimSpace(strings.Split(com.Message, "CHANGELOG:")[1]))
+		}
 	}
 
 	return msgs, nil
