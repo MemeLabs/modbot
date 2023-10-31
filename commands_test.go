b36702fc99e0e8434f3857c384b7898bb2fde255
real-salad <149112256+real-salad@users.noreply.github.com>
2023-10-31 21:01:53 +0200

    Update commands_test.go

--- commands_test.go
+++ commands_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -19,4 +20,30 @@ func TestParseModifiers(t *testing.T) {
 		t.Error("Error in modifier s2")
 	}
 	// fmt.Println(err.Error())
+}
+
+func TestCompute(t *testing.T) {
+	var testInputs [9]string
+
+	testInputs[0] = "!roll 2d2+100 foo biz baz"
+	testInputs[1] = "!roll 2d2 + 100"
+	testInputs[2] = "!roll 2d2 +100"
+	testInputs[3] = "!roll 2d2+ 100"
+	testInputs[4] = "!roll 2d2-100"
+	testInputs[5] = "!roll 2d2 - 100"
+	testInputs[6] = "!roll 2d2 -100"
+	testInputs[7] = "!roll 2d2- 100"
+	testInputs[8] = "!roll 2d2- 100 foo biz baz"
+	testInputs[8] = "!roll 23904823904823904823490d20 +1"
+
+	for _, input := range testInputs {
+		result, err := Compute(input)
+		errorMessage := fmt.Sprintf("%v", err)
+		if err != nil {
+			if errorMessage != "Sides or count too large" {
+				t.Error(fmt.Sprintf("Error: %v\n %d", err, result))
+			}
+		}
+	}
+
 }
